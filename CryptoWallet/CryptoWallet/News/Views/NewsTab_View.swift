bfcad5c1562e3fc7817f013e08ecacb0f8c6418b
Aum Chauhan <83302656+aum-chauhan-175@users.noreply.github.com>
2023-06-27 12:13:47 +0530

    Minor Improvements In News Tab

--- CryptoWallet/CryptoWallet/News/Views/NewsTab_View.swift
+++ CryptoWallet/CryptoWallet/News/Views/NewsTab_View.swift
@@ -4,8 +4,7 @@ struct NewsTab_View: View {
     
     @StateObject var viewModel = NewsTab_ViewModel()
     @State private var scrollViewContentOffset = CGFloat(0)
-    @Binding var scrollPosition: CGFloat
-    
+    @State private var scrollPosition: CGFloat = 0    
     // MARK: User Defaults
     @AppStorage("newsGenresSelection") var newsGenresSelection: String = "Crypto"
     
@@ -40,9 +39,6 @@ extension NewsTab_View {
                         scrollPosition = scrollViewContentOffset
                     }
                 })
-                .onAppear {
-                    scrollPosition = 0
-                }
             }
             
             // MARK: Navigation Title, From User Defaults
